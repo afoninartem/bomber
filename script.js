11e4c349c931a91b40afe51049a8f4f1238ed0c6
afoninartem <47443887+afoninartem@users.noreply.github.com>
2020-07-06 08:18:55 +0300

    upd

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const game = document.querySelector('.game');
 for (let i = 0; i < gameHeight; i += 1) {
   const gameRow = document.createElement('div');
   gameRow.classList.add('game__row');
-  gameRow.dataset.testValue = i;
   game.appendChild(gameRow);
   for (let j = 0; j < gameWidth; j += 1) {
     const gameCell = document.createElement('div');
     gameCell.classList.add('game__cell');
-    gameCell.dataset.testValue = j;
+    gameCell.dataset.row = i;
+    gameCell.dataset.col = j;
     gameRow.appendChild(gameCell);
   }
 }
@@ -34,47 +34,95 @@ for (let i = 0; i < bombs; i += 1) {
   cells[bombed].classList.add('bomb');
 }
 //index of element 
-const getIndex = (x) => {
-  let index = 0;
-  temp = x;
-  while (temp !== x.parentElement.firstElementChild) {
-    temp = temp.previousSibling;
-    index += 1;
+// const getIndex = (x) => {
+//   let index = 0;
+//   temp = x;
+//   while (temp !== x.parentElement.firstElementChild) {
+//     temp = temp.previousSibling;
+//     index += 1;
+//   }
+//   return index;
+// }
+//surround cells
+const surroundCells = cell => {
+  let x = +cell.dataset.col;
+  let y = +cell.dataset.row;
+  let arr = [];
+  for (let i = y - 1; i <= y + 1; i += 1) {
+    for (let j = x - 1; j <= x + 1; j += 1) {
+      arr.push(document.querySelector(`div[data-row='${i}'][data-col='${j}']`));
+    }
   }
-  return index;
+  arr.splice(4, 1);
+  let result = arr.filter(el => el !== null);
+  // console.log(result)
+  return result;
 }
 
-const surroundCells = (cell) => {
-  let x = cell.dataset.testValue;
-  let y = cell.parentElement.dataset.testValue;
-  const arr = [];
-  for (let i = y - 1; i < 3; i += 1) {
-    for (let j = x - 1; j < 3; i += 1) {
-      arr.push(); //разобраться с доступом к атрибуту датасет
-    }
+//coloring function
+const coloring = (cell) => {
+  switch (cell.textContent) {
+    case '0':
+      cell.style.color = 'transparent';
+      break;
+    case '1':
+      cell.style.color = 'blue';
+      break;
+    case '2':
+      cell.style.color = 'green';
+      break;
+    case '3':
+      cell.style.color = 'red';
+      break;
+    case '4':
+      cell.style.color = 'darkblue';
+      break;
+    case '5':
+      cell.style.color = 'darkred';
+      break;
   }
 }
+
 //tips generation
 cells.forEach(cell => {
-  let index = getIndex(cell);
-  if (!cell.classList.contains('bomb')) {
-    let tip = 0;
-    if (cell.previousSibling !== null && cell.previousSibling.classList.contains('bomb')) tip++;
-    if (cell.nextSibling !== null && cell.nextSibling.classList.contains('bomb')) tip++
-    if (cell.parentElement !== game.firstElementChild) {
-      if (cell.parentElement.previousSibling.children[index] !== undefined && cell.parentElement.previousSibling.children[index].classList.contains('bomb')) tip++;
-      if (cell.parentElement.previousSibling.children[index - 1] !== undefined && cell.parentElement.previousSibling.children[index - 1].classList.contains('bomb')) tip++;
-      if (cell.parentElement.previousSibling.children[index + 1] !== undefined && cell.parentElement.previousSibling.children[index + 1].classList.contains('bomb')) tip++;
-    }
-    if (cell.parentElement !== game.lastElementChild) {
-      if (cell.parentElement.nextSibling.children[index] !== undefined && cell.parentElement.nextSibling.children[index].classList.contains('bomb')) tip++;
-      if (cell.parentElement.nextSibling.children[index - 1] !== undefined && cell.parentElement.nextSibling.children[index - 1].classList.contains('bomb')) tip++;
-      if (cell.parentElement.nextSibling.children[index + 1] !== undefined && cell.parentElement.nextSibling.children[index + 1].classList.contains('bomb')) tip++;
-    }
-    if (tip > 0) cell.textContent = tip;
-    else cell.classList.add('empty');
-  }
+  // let index = getIndex(cell);
+  // if (!cell.classList.contains('bomb')) {
+  //   let tip = 0;
+  //   if (cell.previousSibling !== null && cell.previousSibling.classList.contains('bomb')) tip++;
+  //   if (cell.nextSibling !== null && cell.nextSibling.classList.contains('bomb')) tip++
+  //   if (cell.parentElement !== game.firstElementChild) {
+  //     if (cell.parentElement.previousSibling.children[index] !== undefined && cell.parentElement.previousSibling.children[index].classList.contains('bomb')) tip++;
+  //     if (cell.parentElement.previousSibling.children[index - 1] !== undefined && cell.parentElement.previousSibling.children[index - 1].classList.contains('bomb')) tip++;
+  //     if (cell.parentElement.previousSibling.children[index + 1] !== undefined && cell.parentElement.previousSibling.children[index + 1].classList.contains('bomb')) tip++;
+  //   }
+  //   if (cell.parentElement !== game.lastElementChild) {
+  //     if (cell.parentElement.nextSibling.children[index] !== undefined && cell.parentElement.nextSibling.children[index].classList.contains('bomb')) tip++;
+  //     if (cell.parentElement.nextSibling.children[index - 1] !== undefined && cell.parentElement.nextSibling.children[index - 1].classList.contains('bomb')) tip++;
+  //     if (cell.parentElement.nextSibling.children[index + 1] !== undefined && cell.parentElement.nextSibling.children[index + 1].classList.contains('bomb')) tip++;
+  //   }
+  //   if (tip > 0) cell.textContent = tip;
+  //   else cell.classList.add('empty');
+  // }
+  let cellsAround = surroundCells(cell);
+  let bombsAround = cellsAround.filter(el => el.classList.contains('bomb')).length;
+  if (bombsAround > 0) cell.textContent = bombsAround;
+  else if (!cell.classList.contains('bomb')) cell.classList.add('empty');
 });
+
+
+//empty cells
+let emptyCells = document.querySelectorAll('.empty');
+emptyCells.forEach(cell => {
+  cell.addEventListener('click', function(){
+    let targetArea = surroundCells(event.target);
+    targetArea.forEach(c => {
+      coloring(c);
+      c.classList.add('opened');
+      // if (!c.classList.contains('bomb')) c.classList.add('opened'); //некорректно, рядом с пустой всегда нет бомб
+      if (c.classList.contains('empty')) surroundCells(c);
+    })
+  })
+})
 
 //demining 
 cells.forEach(cell => {
@@ -82,26 +130,7 @@ cells.forEach(cell => {
     if (!event.target.classList.contains('flag') && !event.target.classList.contains('bomb')) {
       event.target.classList.add('opened');
       //coloring 
-      switch (event.target.textContent) {
-        case '0':
-          event.target.style.color = 'transparent';
-          break;
-        case '1':
-          event.target.style.color = 'blue';
-          break;
-        case '2':
-          event.target.style.color = 'green';
-          break;
-        case '3':
-          event.target.style.color = 'red';
-          break;
-        case '4':
-          event.target.style.color = 'darkblue';
-          break;
-        case '5':
-          event.target.style.color = 'darkred';
-          break;
-      }
+      coloring(event.target);
     }
     if (!event.target.classList.contains('flag') && event.target.classList.contains('bomb')) {
       const bombCells = document.querySelectorAll('.bomb');
